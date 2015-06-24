0604b5d4388c7ddfe61aceec6b62bcecc1aa2319
cyuseff <cr.yuseff@gmail.com>
2015-06-24 00:53:19 -0300

    test uncommented

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
 //Auth test
-/*require('./auth/local-signin');
+require('./auth/local-signin');
 require('./auth/local-login');
-require('./auth/revokeAllTokens');*/
+require('./auth/revokeAllTokens');
 
 //Users test
 require('./users');
