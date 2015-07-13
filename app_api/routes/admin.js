312fd48ba825fa4b11dcaac8c0ac8b281f2eeb77
cyuseff <cr.yuseff@gmail.com>
2015-07-13 12:48:15 -0300

    checkRoute method added plus defaults admin + rols

--- app_api/routes/admin.js
+++ app_api/routes/admin.js
@@ -4,10 +4,72 @@ var router = require('express').Router()
   , hh = require('../helpers')
   , rolCtrl = require('../controllers/admin/roles');
 
+/* This block should have his own file */
+var Rol = require('../models/rol');
+
 function isAdmin(req, res, next) {
   if(req.user && req.user.admin) return next();
   return hh.sendJsonResponse(res, 403, {message: 'You don\'t have admin privilege!'});
 }
+function checkRoute(route, scope, reqUrl, reqMethod, prefixUrl) {
+
+  var url
+    , regEx
+    , match;
+  console.log(route);
+  console.log('scope: '+scope);
+  console.log('reqUrl: '+reqUrl);
+  console.log('reqMethod: '+reqMethod);
+  console.log('prefixUrl: '+prefixUrl);
+
+  if(scope === '*') scope = '\\w*';
+  if(route.recursive) scope += '(\/\\w*)*';
+
+  url = (prefixUrl + route.url.replace(':scope', scope)).replace(/\//g, '\\/');
+  regEx = new RegExp('^' + url + '$');
+  console.log(url);
+  console.log(regEx);
+  console.log(regEx.exec(reqUrl));
+
+  // test if route match the scoped route
+  match = regEx.exec(reqUrl);
+  if(match) {
+    // check wildcard on methods
+    if(route.methods === '*') {
+      return {url: match[0], method: route.methods, accessLevel: route.accessLevel};
+    } else {
+      if(route.methods.indexOf(reqMethod) !== -1) {
+        return {url: match[0], method: reqMethod, accessLevel: route.accessLevel};
+      } else {
+        return false;
+      }
+    }
+  }
+}
+function isAllowed(req, res, next) {
+
+  var reqUrl = req.originalUrl.split('?')[0]
+    , prefixUrl = '/api/v1/admin/'
+    , url;
+
+  isAdmin(req, res, function() {
+    if(!req.user.roles) return hh.sendJsonResponse(res, 403, {error: 'No roles.'});
+
+    Rol.findOneById(req.user.roles[0].id, function(err, rol) {
+      for(var i=0, l=rol.routes.length; i<l; i++) {
+        var isAllowed = checkRoute(rol.routes[i], req.user.roles[0].scope, reqUrl, req.method, prefixUrl);
+        if(isAllowed) {
+          req.rol = isAllowed;
+          req.rol.name = rol.name;
+          req.rol.accessLevel = req.rol.accessLevel || rol.accessLevel;
+          return next();
+        }
+      }
+      return hh.sendJsonResponse(res, 403, {message: 'You don\'t have admin privilege!'});
+    });
+  });
+}
+/* end */
 
 // Roles
 router.route('/roles')
@@ -20,6 +82,41 @@ router.route('/roles/:rolId')
   .delete(rolCtrl.removeRol);
 
 
+// Providers
+var providers = [
+  {id: '1', name: 'Provider001'},
+  {id: '2', name: 'Provider002'},
+  {id: '3', name: 'Provider003'}
+];
+function getProvider(id) {
+  for(var i=0, l=providers.length; i<l; i++) if(id === providers[i].id) return providers[i];
+  return null;
+}
+router.route('/providers')
+  .get(function(req, res) {
+    hh.sendJsonResponse(res, 200, {providers: providers});
+  });
+router.route('/providers/:providerId')
+  .get(function(req, res) {
+    var provider = getProvider(req.params.providerId);
+    if(!provider) return hh.sendJsonResponse(res, 404, {error: 'Not Found.'});
+    return hh.sendJsonResponse(res, 200, {provider: provider, user: req.user, rol: req.rol});
+  });
+router.route('/providers/:providerId/nested')
+  .get(function(req, res) {
+    var provider = getProvider(req.params.providerId);
+    if(!provider) return hh.sendJsonResponse(res, 404, {error: 'Not Found.'});
+    return hh.sendJsonResponse(res, 200, {provider: provider, user: req.user, rol: req.rol});
+  });
+
+
+// Clubs
+router.route('/clubs')
+  .get(function(req, res) {
+    return hh.sendJsonResponse(res, 200, {message: 'Welcome to clubs', user: req.user, rol: req.rol});
+  });
+
+
 module.exports = function(app) {
-  app.use('/api/v1/admin', hh.authToken, isAdmin, router);
+  app.use('/api/v1/admin', hh.authToken, isAllowed, router);
 };
--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,49 @@
 'use strict';
 
 var User = require('../app_api/models/user')
+  , Rol = require('../app_api/models/rol')
   , PASSWORD = '123456'
   , NAME = 'GotIt'
   , LAST_NAME = 'Admin'
-  , EMAIL = 'admin@gotit.com';
+  , EMAIL = 'admin@gotit.com'
+  , P_NAME = 'Provider'
+  , P_LAST_NAME = 'Admin'
+  , P_EMAIL = 'provider-admin@gotit.com';
+
+var rol, rol2;
+
+Rol.findAll(function(err, roles) {
+  if(!roles.length) {
+    rol = new Rol({
+      name: 'superAdmin',
+      accessLevel: 1,
+      routes: [
+        {
+          url: ':scope',
+          methods: '*',
+          recursive: true
+        }
+      ]
+    });
+    rol.save();
+
+    rol2 = new Rol({
+      name: 'providerAdmin',
+      accessLevel: 2,
+      routes: [
+        {
+          url: 'providers/:scope',
+          methods: '*',
+          recursive: true
+        }
+      ]
+    });
+    rol2.save();
+  }
+});
 
 User
-  .findOne({fullName: 'GotIt Admin'})
+  .findOne({fullName: NAME +' '+ LAST_NAME})
   .exec(function(err, admin) {
     if(err) console.log(err);
     if(admin) return console.log('Admin Found!');
@@ -33,10 +69,59 @@ User
       // set as Admin
       user.admin = true;
 
+      // Assign SuperAdmin Rol
+      user.roles.push({
+        id: rol.id,
+        scope: '*'
+      });
+
       // save user before serialize into his token
       user.save(function(err) {
         if(err) return console.log(err);
         console.log('Admin Created!');
+      });
+
+    });
+
+  });
+
+User
+  .findOne({fullName: P_NAME +' '+ P_LAST_NAME})
+  .exec(function(err, admin) {
+    if(err) console.log(err);
+    if(admin) return console.log('Provider Admin Found!');
+
+    // Create a new User
+    var user = new User();
+    user.generateHash(PASSWORD, function(err, hash) {
+
+      if(err) return console.log(err);
+
+      // Push to users emails array
+      user.emails.push(P_EMAIL);
+
+      // add general properties
+      user.firstName = P_NAME;
+      user.lastName = P_LAST_NAME;
+      user.fullName = P_NAME + ' ' + P_LAST_NAME;
+
+      // add strategy properties
+      user.local.email = P_EMAIL;
+      user.local.password = hash;
+
+      // set as Admin
+      user.admin = true;
+
+      // Assign SuperAdmin Rol
+      user.roles.push({
+        id: rol2.id,
+        scope: '3'
+      });
+
+      // save user before serialize into his token
+      user.save(function(err) {
+        if(err) return console.log(err);
+        console.log('Provider Admin Created!');
       });
 
     });
